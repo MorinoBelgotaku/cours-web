968fab94bc7e54172fd46da1c55fe2b6b63296c9
Morino_Belgotaku <morinbruno@hotmail.be>
2023-11-26 20:44:24 +0100

    Update

--- Site 3/js/script.js
+++ Site 3/js/script.js
@@ -1,15 +1,10 @@
-// Retour au top
-function return_top() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
-
 // SNK
 const audio_snk_op = new Audio("./audio/snk_op.mp3");
 const id_snk = document.getElementById("snk");
 
 id_snk.addEventListener("mouseover", () => {
     audio_snk_op.currentTime = 67;
-    audio_snk_op.volume = 0.3;
+    audio_snk_op.volume = 0.1;
     audio_snk_op.play();
     control.textContent = 'Pause';
 });
@@ -29,7 +24,7 @@ const id_sao = document.getElementById("sao");
 
 id_sao.addEventListener("mouseover", () => {
     audio_sao_op.currentTime = 45;
-    audio_sao_op.volume = 0.3;
+    audio_sao_op.volume = 0.1;
     audio_sao_op.play();
     control.textContent = 'Pause';
 });
@@ -49,7 +44,7 @@ const id_ditf = document.getElementById("ditf");
 
 id_ditf.addEventListener("mouseover", () => {
     audio_ditf_op.currentTime = 56;
-    audio_ditf_op.volume = 0.3;
+    audio_ditf_op.volume = 0.1;
     audio_ditf_op.play();
     control.textContent = 'Pause';
 });
@@ -62,3 +57,64 @@ id_ditf.addEventListener("mouseout", () => {
 id_ditf.addEventListener("click", () => {
     window.open('https://www.crunchyroll.com/fr/series/GY8VEQ95Y/darling-in-the-franxx', '_blank');
 });
+
+// TEIS
+const audio_teis_op = new Audio("./audio/teis_op.mp3");
+const id_teis = document.getElementById("teis");
+
+id_teis.addEventListener("mouseover", () => {
+    audio_teis_op.currentTime = 57;
+    audio_teis_op.volume = 0.1;
+    audio_teis_op.play();
+    control.textContent = 'Pause';
+});
+
+id_teis.addEventListener("mouseout", () => {
+    audio_teis_op.pause();
+    control.textContent = 'Play';
+}); 
+
+id_teis.addEventListener("click", () => {
+    window.open('https://animationdigitalnetwork.fr/video/the-eminence-in-shadow', '_blank');
+});
+
+// REZERO
+const audio_rezero_op = new Audio("./audio/rezero_op.mp3");
+const id_rezero = document.getElementById("rezero");
+
+id_rezero.addEventListener("mouseover", () => {
+    audio_rezero_op.currentTime = 55;
+    audio_rezero_op.volume = 0.1;
+    audio_rezero_op.play();
+    control.textContent = 'Pause';
+});
+
+id_rezero.addEventListener("mouseout", () => {
+    audio_rezero_op.pause();
+    control.textContent = 'Play';
+}); 
+
+id_rezero.addEventListener("click", () => {
+    window.open('https://www.crunchyroll.com/fr/series/GRGG9798R/rezero--starting-life-in-another-world-', '_blank');
+});
+
+// Made in Abyss
+const audio_mia_op = new Audio("./audio/mia_op.mp3");
+const id_mia = document.getElementById("mia");
+
+id_mia.addEventListener("mouseover", () => {
+    audio_mia_op.currentTime = 47;
+    audio_mia_op.volume = 0.1;
+    audio_mia_op.play();
+    control.textContent = 'Pause';
+});
+
+id_mia.addEventListener("mouseout", () => {
+    audio_mia_op.pause();
+    control.textContent = 'Play';
+}); 
+
+id_mia.addEventListener("click", () => {
+    window.open('https://www.crunchyroll.com/fr/series/GQWH0M77N/made-in-abyss', '_blank');
+});
+
---
+++ Site 3/js/top.js
@@ -0,0 +1,4 @@
+// Retour au top
+function return_top() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
